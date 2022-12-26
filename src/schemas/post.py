c1102c184f0cfd17ffd8ff6120ebfbae0c35bb47
FOMSI <ihor.fomin.mypage@gmail.com>
2022-12-26 19:35:31 +0200

    Improved object validation. Working with jsonschema.

---
+++ src/schemas/post.py
@@ -0,0 +1,10 @@
+POST_SCHEMA = {
+    "type": "object",
+    "properties": {
+        "id": {"type": "number"},
+        "title": {"type": "string"}
+    },
+    "required": ["id"]
+}
+
+# {'id': 1, 'title': 'Post 1'}
--- tests/test_something.py
+++ tests/test_something.py
@@ -3,11 +3,13 @@ import requests
 from configuration import SERVICE_URL
 
 from src.enums.global_enums import GlobalErrorMessages
+from src.baseclasses.response import Response
+from src.schemas.post import POST_SCHEMA
 
 
 def test_getting_posts():
-    response = requests.get(url=SERVICE_URL)
-    received_data = response.json()
+    r = requests.get(url=SERVICE_URL)
+    response = Response(r)
+    response.assert_status_code(200).validate(POST_SCHEMA)
 
-    assert response.status_code == 200, GlobalErrorMessages.WRONG_STATUS_CODE.value
-    assert len(received_data) == 3, GlobalErrorMessages.WRONG_ELEMENT_COUNT.value
+# [{'id': 1, 'title': 'Post 1'}, {'id': 2, 'title': 'Post 2'}, {'id': 3, 'title': 'Post 3'}]
