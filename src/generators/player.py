74295a4aab999acf6fb44b43dfce970d461c6667
FOMSI <ihor.fomin.mypage@gmail.com>
2023-03-09 16:15:43 +0200

    Lessons create builder in PyTest.

---
+++ src/generators/player.py
@@ -0,0 +1,47 @@
+from src.enums.user_enums import Statuses
+
+from src.generators.player_localization import PlayerLocalization
+
+
+class Player:
+
+    def __init__(self):
+        self.result = {}
+        self.reset()
+
+    
+    def set_status(self, status=Statuses.active.value):
+        self.result['account_status'] = status
+        return self
+    
+
+    def set_balance(self, balance=0):
+        self.result['balance'] = balance
+        return self
+    
+
+    def set_avatar(self, avatar='https://google.com/'):
+        self.result['avatar'] = avatar
+        return self
+    
+    def reset(self):
+        self.set_status()
+        self.set_balance()
+        self.set_avatar()
+        self.result["localize"] = {
+                "en": PlayerLocalization('en_US').build(),
+                "ru": PlayerLocalization('ru_RU').build()
+            }
+        return self
+    
+
+    def update_inner_generator(self, key, generator):
+        self.result[key] = {"en": generator.build()}
+        return self
+
+
+    def build(self):
+        return self.result
+    
+
+
--- tests/something/test_something.py
+++ tests/something/test_something.py
@@ -1,4 +1,43 @@
+import requests
+import pytest
 
-def test_something(get_number):
-    assert 1 == 1
-    print(get_number)
+from src.generators.player_localization import PlayerLocalization
+
+
+@pytest.mark.parametrize("status", [
+    "ACTIVE",
+    "BANNED",
+    "DELETED",
+    "INACTIVE"
+])
+def test_something_1(status, get_player_generator):
+    print(get_player_generator.set_status(status).build())
+
+
+# @pytest.mark.parametrize("balance_value", [
+#     "100",
+#     "0",
+#     "-10",
+#     "ddddd"
+# ])
+# def test_something(balance_value, get_player_generator):
+#     print(get_player_generator.set_balance(balance_value).build())
+
+
+@pytest.mark.parametrize("delete_key", [
+    "account_status",
+    "balance",
+    "localize",
+    "avatar"
+])
+def test_something_2(delete_key, get_player_generator):
+    object_to_send = get_player_generator.build()
+    del object_to_send[delete_key]
+    print(object_to_send)
+    
+
+def test_something_3(get_player_generator):
+    object_to_send = get_player_generator.update_inner_generator(
+        'localize', PlayerLocalization('fr_FR').set_number(15)
+        ).build()
+    print(object_to_send)
